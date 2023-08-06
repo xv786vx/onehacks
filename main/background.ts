e22b8657f95f5a3059d8cbe98b1c41e3ce45efe6
Cy1der <67352978+Cy1der@users.noreply.github.com>
2023-08-06 16:31:27 -0400

    remove testing stuff, we starting fr

--- main/background.ts
+++ main/background.ts
@@ -2,8 +2,6 @@ import { app, ipcMain } from "electron";
 import serve from "electron-serve";
 import { createWindow } from "./helpers";
 
-import testNotification from "./example";
-
 const isProd: boolean = process.env.NODE_ENV === "production";
 
 if (isProd) serve({ directory: "app" });
@@ -19,11 +17,6 @@ else app.setPath("userData", `${app.getPath("userData")} (development)`);
 
   if (isProd) await mainWindow.loadURL("app://./home.html");
   else await mainWindow.loadURL(`http://localhost:${process.argv[2]}/home`);
-
-  ipcMain.handle("test-notification", (_, inputTitle, inputBody) => {
-    const result = testNotification(inputTitle, inputBody);
-    return result;
-  });
 })();
 
 app.on("window-all-closed", () => app.quit());
--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,32 +1,13 @@
 import React, { useState } from "react";
 import Head from "next/head";
-import { ipcRenderer } from "electron";
+// import { ipcRenderer } from "electron";
 
 export default function Home() {
-  const [inputTitle, setInputTitle] = useState("");
-  const [inputBody, setInputBody] = useState("");
-
-  const handleTestNotification = async () =>
-    await ipcRenderer.invoke("test-notification", inputTitle, inputBody);
-
   return (
     <React.Fragment>
       <Head>
         <title>Home</title>
       </Head>
-      <span>Title</span>
-      <input
-        className="text-black"
-        value={inputTitle}
-        onChange={(e) => setInputTitle(e.target.value)}
-      />
-      <span>Body</span>
-      <input
-        className="text-black"
-        value={inputBody}
-        onChange={(e) => setInputBody(e.target.value)}
-      />
-      <button onClick={handleTestNotification}>Send Notification</button>
     </React.Fragment>
   );
 }
