b1022778c60d6cb074718c4171232f233fb1f999
cy1der <mrvenomousgd@gmail.com>
2023-08-06 14:06:27 -0400

    code cleanup

--- main/background.ts
+++ main/background.ts
@@ -1,32 +1,22 @@
-import { app } from 'electron';
-import serve from 'electron-serve';
-import { createWindow } from './helpers';
+import { app } from "electron";
+import serve from "electron-serve";
+import { createWindow } from "./helpers";
 
-const isProd: boolean = process.env.NODE_ENV === 'production';
+const isProd: boolean = process.env.NODE_ENV === "production";
 
-if (isProd) {
-  serve({ directory: 'app' });
-} else {
-  app.setPath('userData', `${app.getPath('userData')} (development)`);
-}
+if (isProd) serve({ directory: "app" });
+else app.setPath("userData", `${app.getPath("userData")} (development)`);
 
 (async () => {
   await app.whenReady();
 
-  const mainWindow = createWindow('main', {
+  const mainWindow = createWindow("main", {
     width: 1000,
     height: 600,
   });
 
-  if (isProd) {
-    await mainWindow.loadURL('app://./home.html');
-  } else {
-    const port = process.argv[2];
-    await mainWindow.loadURL(`http://localhost:${port}/home`);
-    mainWindow.webContents.openDevTools();
-  }
+  if (isProd) await mainWindow.loadURL("app://./home.html");
+  else await mainWindow.loadURL(`http://localhost:${process.argv[2]}/home`);
 })();
 
-app.on('window-all-closed', () => {
-  app.quit();
-});
+app.on("window-all-closed", () => app.quit());
