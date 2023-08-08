e05c9a9f74acdf8feb4b07f495dde0ac4a5de1d7
cy1der <mrvenomousgd@gmail.com>
2023-08-07 22:01:46 -0400

    better testimonials

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -22,7 +22,6 @@ export default function Home() {
       </Head>
 
       <div className="bg-black1 h-screen flex flex-row">
-
         <div className="w-1/2">
           <div className="logo">
             <Image src={logo} />
@@ -41,24 +40,43 @@ export default function Home() {
             </div>
           </Link>
         </div>
-        
+
         <div className="w-1/2 pl-16">
           <div>
             <h1 className="testimonial-title">Testimonials</h1>
             <div className="custom-scrollbar overflow-y-scroll max-h-[560px]">
-              <p className="quote">"Helped me zero in on my homework with clear time blocks, I got my stuff done and stopped procrastinating!"</p>
-              <p className="quote">"Helped me zero in on my homework with clear time blocks, I got my stuff done and stopped procrastinating!"</p>
-              <p className="quote">"Helped me zero in on my homework with clear time blocks, I got my stuff done and stopped procrastinating!"</p>
-              <p className="quote">"Helped me zero in on my homework with clear time blocks, I got my stuff done and stopped procrastinating!"</p>
-              <p className="quote">"Helped me zero in on my homework with clear time blocks, I got my stuff done and stopped procrastinating!"</p>
-              <p className="quote">"Helped me zero in on my homework with clear time blocks, I got my stuff done and stopped procrastinating!"</p>
+              <p className="quote">
+                "Helped me hone in on my homework with clear time blocks, I got
+                my stuff done and stopped procrastinating!"
+              </p>
+              <p className="quote">
+                "Sage helped me spend more time working on what I actually
+                needed to do, instead of wasting time on trying to figure out
+                how I need to do it."
+              </p>
+              <p className="quote">
+                "Sage's time blocks helped me a ton with increasing my
+                productivity! I feel less stressed when I have a lot of work to
+                do knowing that Sage will help me ensure I spend my time
+                wisely."
+              </p>
+              <p className="quote">
+                "It amazes me how accurate Sage's generated time blocks are,
+                they've helped me break out of my procrastinating habits and get
+                me started on effective homework and studying."
+              </p>
+              <p className="quote">
+                "Sage is awesome for when life gets too hectic and you need a
+                second mind to handle what matters less."
+              </p>
+              <p className="quote">
+                "I would recommend Sage to any of my fellow students, especially
+                ones struggling with managing their time."
+              </p>
             </div>
           </div>
-
         </div>
-
       </div>
-
     </React.Fragment>
   );
 }
--- renderer/pages/next.tsx
+++ renderer/pages/next.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Head from "next/head";
-import Link from "next/link"
+import Link from "next/link";
 import { Container } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -41,16 +41,20 @@ export default function AddTask() {
 
     return (
       <>
-      <div>
-        <button
-          onClick={handleClick}
-          className={inputName.length === 0 || inputDate.length === 0 ? "task-btn-disabled" : "task-btn"}
-          disabled={inputName.length === 0 || inputDate.length === 0}
-        >
-          Create Task
-        </button>
-        {task ? <div>hello</div> : <div></div>}
-      </div>
+        <div>
+          <button
+            onClick={handleClick}
+            className={
+              inputName.length === 0 || inputDate.length === 0
+                ? "task-btn-disabled"
+                : "task-btn"
+            }
+            disabled={inputName.length === 0 || inputDate.length === 0}
+          >
+            Create Task
+          </button>
+          {task ? <div>hello</div> : <></>}
+        </div>
       </>
     );
   };
@@ -63,162 +67,160 @@ export default function AddTask() {
 
       <div className="bg-black1 h-screen">
         <div className="w-96 pl-8 pt-12">
+          <Link href="/home">
+            <button className="back-btn">Back</button>
+          </Link>
 
-      <Link href="/home">
-        <button className="back-btn">Back</button>
-      </Link>
+          <div className="container pb-4">
+            <button className="toggle-btn" onClick={handleToggle}>
+              {isOpen ? "-" : "+"}
+            </button>
+            {isOpen && (
+              <div className="bg-black1 border border-sage2 rounded-[8px] w-84 mt-4 p-4">
+                <div className="flex p-4 justify-center items-center ">
+                  <form className="flex flex-col">
+                    <div className="pb-4">
+                      <input
+                        className="input-field"
+                        placeholder="Task Name"
+                        value={inputName}
+                        onChange={(e) => setInputName(e.target.value)}
+                      />
+                    </div>
 
-      <div className="container pb-4">
-        <button
-          className="toggle-btn"
-          onClick={handleToggle}
-        >
-          {isOpen ? "-" : "+"}
-        </button>
-        {isOpen && (
-          <div className="bg-black1 border border-sage2 rounded-[8px] w-84 mt-4 p-4">
-            <div className="flex p-4 justify-center items-center ">
-              <form className="flex flex-col">
-                <div className="pb-4">
+                    <div className="pb-4">
+                      <label
+                        className="block scp text-md pb-2"
+                        htmlFor="duedate"
+                      >
+                        Due Date
+                      </label>
 
-                  <input
-                    className="input-field"
-                    placeholder="Task Name"
-                    value={inputName}
-                    onChange={(e) => setInputName(e.target.value)}
-                  />
+                      <React.Fragment>
+                        <Container>
+                          <div className="input-field">
+                            <DatePicker
+                              selected={inputDate}
+                              onChange={(date: React.SetStateAction<string>) =>
+                                setInputDate(date)
+                              }
+                              placeholderText="Select date"
+                              dateFormat="MMMM d, yyyy h:mm aa"
+                              showPopperArrow={false}
+                              showTimeSelect
+                              timeFormat="HH:mm"
+                              timeIntervals={15}
+                              className="p-2 w-52 -my-2 -ml-2 text-white text-align-left bg-black1 hover:bg-black2 duration-300"
+                            />
+                          </div>
+                        </Container>
+                      </React.Fragment>
+                    </div>
+
+                    <div className="pb-4">
+                      <label
+                        className="block scp text-md pb-2"
+                        htmlFor="priority"
+                      >
+                        Priority
+                      </label>
+
+                      <select
+                        name="priority"
+                        value={inputPriority}
+                        onChange={(e) => {
+                          setInputPriority(e.target.value);
+                          console.log(inputPriority);
+                        }}
+                        className="input-field w-full"
+                      >
+                        <option>Select an option</option>
+                        <option>Low</option>
+                        <option>Medium</option>
+                        <option>High</option>
+                      </select>
+                    </div>
+
+                    <div>
+                      <label
+                        className="block scp text-md pb-2"
+                        htmlFor="progress"
+                      >
+                        Progress
+                      </label>
+
+                      <input
+                        type="range"
+                        step="10"
+                        value={inputProgress}
+                        onChange={(e) =>
+                          setInputProgress(parseInt(e.target.value))
+                        }
+                        className="custom-slider"
+                      ></input>
+                      <span className="scp pt-2 text-center">
+                        {inputProgress}%
+                      </span>
+                    </div>
+
+                    <div className="flex pt-8 mx-auto justify-center">
+                      <button type="submit" onClick={handleAddTask}>
+                        <CreateTask />
+                      </button>
+                    </div>
+                  </form>
                 </div>
+              </div>
+            )}
+          </div>
 
-                <div className="pb-4">
-                  <label
-                    className="block scp text-md pb-2"
-                    htmlFor="duedate"
-                  >
-                    Due Date
-                  </label>
+          <div className="flex flex-row gap-4">
+            <div className="pb-4">
+              <React.Fragment>
+                <Container>
+                  <div className="input-field">
+                    <DatePicker
+                      selected={inputStartDate}
+                      onChange={(date: React.SetStateAction<string>) =>
+                        setInputStartDate(date)
+                      }
+                      placeholderText="Start Study"
+                      dateFormat="MMMM d, yyyy h:mm aa"
+                      showPopperArrow={false}
+                      showTimeSelect
+                      timeFormat="HH:mm"
+                      timeIntervals={15}
+                      className="p-2 w-32 -my-2 -ml-2 text-white text-center bg-black1 hover:bg-black2 duration-300"
+                    />
+                  </div>
+                </Container>
+              </React.Fragment>
+            </div>
 
-                  <React.Fragment>
-                    <Container>
-                      <div className="input-field">
-                        <DatePicker
-                          selected={inputDate}
-                          onChange={(date: React.SetStateAction<string>) =>
-                            setInputDate(date)
-                          }
-                          placeholderText="Select date"
-                          dateFormat="MMMM d, yyyy h:mm aa"
-                          showPopperArrow={false}
-                          showTimeSelect
-                          timeFormat="HH:mm"
-                          timeIntervals={15}
-                          className="p-2 w-52 -my-2 -ml-2 text-white text-align-left bg-black1 hover:bg-black2 duration-300"
-                        />
-                      </div>
-                    </Container>
-                  </React.Fragment>
-                </div>
-
-                <div className="pb-4">
-                  <label
-                    className="block scp text-md pb-2"
-                    htmlFor="priority"
-                  >
-                    Priority
-                  </label>
-
-                  <select
-                    name="priority"
-                    value={inputPriority}
-                    onChange={(e) => {
-                      setInputPriority(e.target.value);
-                      console.log(inputPriority);
-                    }}
-                    className="input-field w-full"
-                  >
-                    <option>Select an option</option>
-                    <option>Low</option>
-                    <option>Medium</option>
-                    <option>High</option>
-                  </select>
-                </div>
-
-                <div>
-                  <label
-                    className="block scp text-md pb-2"
-                    htmlFor="progress"
-                  >
-                    Progress
-                  </label>
-
-                  <input
-                    type="range"
-                    step="10"
-                    value={inputProgress}
-                    onChange={(e) => setInputProgress(parseInt(e.target.value))}
-                    className="custom-slider"
-                  ></input>
-                  <span className="scp pt-2 text-center">{inputProgress}%</span>
-                </div>
-
-                <div className="flex pt-8 mx-auto justify-center">
-                  <button type="submit" onClick={handleAddTask}>
-                    <CreateTask />
-                  </button>
-                </div>
-              </form>
+            <div className="pb-4">
+              <React.Fragment>
+                <Container>
+                  <div className="input-field">
+                    <DatePicker
+                      selected={inputEndDate}
+                      onChange={(date: React.SetStateAction<string>) =>
+                        setInputEndDate(date)
+                      }
+                      placeholderText="End Study"
+                      dateFormat="MMMM d, yyyy h:mm aa"
+                      showPopperArrow={false}
+                      showTimeSelect
+                      timeFormat="HH:mm"
+                      timeIntervals={15}
+                      className="p-2 w-32 -my-2 -ml-2 text-white text-center bg-black1 hover:bg-black2 duration-300"
+                    />
+                  </div>
+                </Container>
+              </React.Fragment>
             </div>
           </div>
-        )}
-      </div>
-
-      <div className="flex flex-row gap-4">
-        <div className="pb-4">
-          <React.Fragment>
-            <Container>
-              <div className="input-field">
-                <DatePicker
-                  selected={inputStartDate}
-                  onChange={(date: React.SetStateAction<string>) =>
-                    setInputStartDate(date)
-                  }
-                  placeholderText="Start Study"
-                  dateFormat="MMMM d, yyyy h:mm aa"
-                  showPopperArrow={false}
-                  showTimeSelect
-                  timeFormat="HH:mm"
-                  timeIntervals={15}
-                  className="p-2 w-32 -my-2 -ml-2 text-white text-center bg-black1 hover:bg-black2 duration-300"
-                />
-              </div>
-            </Container>
-          </React.Fragment>
-        </div>
-
-        <div className="pb-4">
-          <React.Fragment>
-            <Container>
-              <div className="input-field">
-                <DatePicker
-                  selected={inputEndDate}
-                  onChange={(date: React.SetStateAction<string>) =>
-                    setInputEndDate(date)
-                  }
-                  placeholderText="End Study"
-                  dateFormat="MMMM d, yyyy h:mm aa"
-                  showPopperArrow={false}
-                  showTimeSelect
-                  timeFormat="HH:mm"
-                  timeIntervals={15}
-                  className="p-2 w-32 -my-2 -ml-2 text-white text-center bg-black1 hover:bg-black2 duration-300"
-                />
-              </div>
-            </Container>
-          </React.Fragment>
         </div>
       </div>
-      </div>
-      </div>
-
     </React.Fragment>
   );
 }
