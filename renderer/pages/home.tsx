c20621bbc96e0caec515f61f588b657b16f52066
Cy1der <67352978+Cy1der@users.noreply.github.com>
2023-08-07 20:01:31 -0400

    add font

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,36 +1,43 @@
 import React from "react";
 import Head from "next/head";
 import Link from "next/link";
-import Image from "next/image"
-import logo from '../assets/onehacks-logo.png'
+import Image from "next/image";
+import logo from "../assets/onehacks-logo.png";
 
 export default function Home() {
-
   return (
     <React.Fragment>
       <Head>
-        <title>Sage.</title>
+        <title>Sage</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@500&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-    <div className="bg-black1 h-screen">
-
-      <div className="logo">
-        <Image src={logo} />
-      </div>
-
-      <div className="font-custom bg-black1 pl-24 sm:pl-36 md:pl-48 pt-16">
-        <h1 className="pb-4 text-6xl font-bold">Sage.</h1>
-        <p className="text-2xl">Focus on what really matters.</p>
-      </div>
-
-      <Link href="/next">
-        <div className="bg-black1 pl-24 sm:pl-36 md:pl-48 py-24">
-          <button className="homepage-btn">
-            <a className="font-custom">Create Tasks</a>
-          </button>
+      <div className="bg-black1 h-screen">
+        <div className="logo">
+          <Image src={logo} />
         </div>
-      </Link>
-    </div>
 
+        <div>
+          <h1 className="sage-title">Sage.</h1>
+          <p className="sage-slogan">Focus on what really matters.</p>
+        </div>
+
+        <Link href="/next">
+          <div className="bg-black1 pl-24 sm:pl-36 md:pl-48 py-24">
+            <button className="homepage-btn">
+              <a className="font-custom">Create Tasks</a>
+            </button>
+          </div>
+        </Link>
+      </div>
     </React.Fragment>
   );
 }
